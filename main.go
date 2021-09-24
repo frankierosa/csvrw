8cab2f42b74e1f89a905c86ca27c2369b580e28e
frankierosa <frankie_rosa@hotmail.com>
2021-09-24 08:19:53 -0400

    push from repo

--- main.go
+++ main.go
@@ -8,6 +8,17 @@ import (
 )
 
 var file = "data/sample.csv"
+var data = make(map[int][]string)
+
+ type Person struct {
+	 ID int
+	 FirstName, LastName, CompanyName, Address, City, County, State, ZipCode, Phone, Email string
+
+ }
+
+ func (p Person) customerData(c map[int]string){
+	 
+ }
 
 func main() {
 	f, err := csvreader.CSVreader(file)
@@ -16,6 +27,7 @@ func main() {
 	}
 	fmt.Println(f)
 	for i, v := range f {
+		append(slice []Type, elems ...Type)
 		fmt.Println(i, v)
 	}
 }
