9e26fda91f4e8231653bd183f62c0de0a263d8b8
Frankie Rosa <frankie_rosa@hotmail.com>
2021-10-04 21:23:49 -0400

    csvfm will collect column and deplay it

--- csvfm/csvfm.go
+++ csvfm/csvfm.go
@@ -1,35 +1,28 @@
 package csvfm
 
 import (
-    "fmt"
-    "os"
-    "strings"
-    "text/tabwriter"
+	"fmt"
+	"strings"
+
+	"github.com/ryanuber/columnize"
 )
 
 var (
     columns []string
-
 )
 
 //CSVFormat display the content separe for each csv columns
 // example form https://networkbit.ch/golang-column-print/
 func CSVFormat(data [][]string) {
-    
-    // initialize tabwrite
-    w := new(tabwriter.Writer)
 
-    // minwidth, tabwidth, padding, padchar, flags
-    w.Init(os.Stdout, 8, 8, 0, 't', 0)
-
-    defer w.Flush()
-
-    // get columns
-    for _, d := range data[0][:] {
-        columns = append(columns, d)
+    // Inserting csv columns with "|"
+     for i := 0; i < 1; i++ {
+        c := strings.Join(data[0][:], " | ")
+        columns = append(columns, c)
     }
 
-    for _, c := range columns {
-        fmt.Println(strings.Trim(fmt.Sprint(c), "[]"))
-    }
+    // Columnize Columns from slice columns
+    // For more detail go to https://github.com/ryanuber/columnize
+    result := columnize.SimpleFormat(columns)
+    fmt.Println(result)
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/frankierosa/csvrw
 
 go 1.17
+
+require github.com/ryanuber/columnize v2.1.2+incompatible // indirect
