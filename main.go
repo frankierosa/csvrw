539a4a635d1e37ded2ae5e7edc7f0df19de2e168
Frankie Rosa <frankie_rosa@hotmail.com>
2021-10-05 22:14:17 -0400

    adding progress bar

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"os"
 	"path/filepath"
 	"strings"
+	"time"
 
 	"github.com/frankierosa/csvrw/asciiart"
 	"github.com/frankierosa/csvrw/csvfm"
@@ -22,13 +23,24 @@ var (
 	file = "data/sample.csv"
 )
 
+// renderbar func for Progress bar
+func renderbar(count, total int) {
+    barwidth := 30
+    done := int(float64(barwidth) * float64(count) / float64(total))
+
+    fmt.Printf("Progress: \x1b[33m%3d%%\x1b[0m ", count*100/total)
+    fmt.Printf("[%s%s]",
+        strings.Repeat("=", done),
+        strings.Repeat("-", barwidth-done))
+}
+
 func main() {
 	// Loading ascii art.
 	asciiart.PrintAsciiArt()
 
 	for {
 		fmt.Print("Enter the csv file location: ")
-		
+
 	    // Reading from user input
 		reader := bufio.NewReader(os.Stdin)
 
@@ -46,6 +58,16 @@ func main() {
 
 		// Validate file ext before passing into CSVReader func.
 		if fileExt == ".csv" || fileExt == ".CSV" {
+			fmt.Println()
+			total := 50
+            for i := 1; i <= total; i++ {
+			//<ESC> means ASCII "escape" character, 0x1B
+            fmt.Print("\x1b7")   // Save the cursor position, save the cursor and Attrs < ESC > 7
+            fmt.Print("\x1b[2k") // Clear the content erase line of the current line < ESC > [2K]
+            renderbar(i, total)
+            time.Sleep(50 * time.Millisecond)
+            fmt.Print("\x1b8") // Recovery cursor position recovery cursor and Atrs < ESC > 8
+        }
 			file, err := csvreader.CSVReader(file)
 	        if err != nil {
 		        log.Fatal(err)
