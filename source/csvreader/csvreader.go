e5da9f83c81c6358b0932377c00d2eca1d2c23bb
Frankie Rosa <frankie_rosa@hotmail.com>
2022-06-07 12:54:05 -0400

    removing bar
    removing progress bar

--- source/csvreader/csvreader.go
+++ source/csvreader/csvreader.go
@@ -1,4 +1,26 @@
-package csvreader
+/*
+ * This file is part of csvrw
+ * Copyright 2022 frankierosa
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/* 
+ * csvreader.go:
+ */
+ 
+ package csvreader
 
 import (
 	"bytes"
